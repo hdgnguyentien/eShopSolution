8ea7d21cc6287130665616d8d8bf940544f2a151
Nguyễn Công Tiến <hdgnguyentien@gmail.com>
2023-02-18 01:03:27 +0700

    Merge pull request #3 from hdgnguyentien/feature/database_design
    Feature/database design

---
+++ eShopSolution.Data/Migrations/20230217175736_SeedData2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class SeedData2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeoAlias",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2023, 2, 18, 0, 57, 36, 479, DateTimeKind.Local).AddTicks(8459));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SeoAlias",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2023, 2, 18, 0, 54, 1, 769, DateTimeKind.Local).AddTicks(7744));
+        }
+    }
+}
